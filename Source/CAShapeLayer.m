fe0ea59de05c1773cf557b7ab5b3c9c956efa8e3
Yonghui Chen <chyhfj@gmail.com>
2014-08-07 23:09:48 +0800

    set display for shape layer

--- Source/CAShapeLayer.m
+++ Source/CAShapeLayer.m
@@ -1,26 +1,5 @@
 /* CAShapeLayer.m
 
-   Copyright (C) 2012 Free Software Foundation, Inc.
-
-   Author: Amr Aboelela <amraboelela@gmail.com>
-
-   This file is part of QuartzCore.
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; see the file COPYING.LIB.
-   If not, see <http://www.gnu.org/licenses/> or write to the
-   Free Software Foundation, 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
 */
 
 #import "CAShapeLayer.h"
@@ -87,6 +66,7 @@
 - (void)setFillColor:(CGColorRef)fillColor
 {
     _fillColor = CGColorRetain(fillColor);
+    [self setNeedsDisplay];
 }
 
 - (CGColorRef)fillColor
@@ -97,6 +77,7 @@
 - (void)setStrokeColor:(CGColorRef)strokeColor
 {
     _strokeColor = CGColorRetain(strokeColor);
+    [self setNeedsDisplay];
 }
 
 - (CGColorRef)strokeColor
@@ -107,6 +88,7 @@
 - (void)setPath:(CGPathRef)path
 {
     _path = CGPathRetain(path);
+    [self setNeedsDisplay];
 }
 
 - (CGPathRef)path
